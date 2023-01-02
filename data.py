ed95dc8309153cfc9271fcb98c35e899a5bfb811
reign12 <3180105232@zju.edu.cn>
2023-01-02 08:46:21 +0000

    Unify to AutoTokenizer is enough

--- data.py
+++ data.py
@@ -5,7 +5,7 @@ from torch.utils.data import Dataset,DataLoader
 from omegaconf import DictConfig
 
 # Support GLM, BART, T5
-from transformers import AutoTokenizer, T5Tokenizer, BartTokenizer
+from transformers import AutoTokenizer
 # Support commonsense_qa, multi_news
 from datasets import load_dataset
 
@@ -15,22 +15,11 @@ from promptsource.templates import DatasetTemplates
 class PGDataset(Dataset):
     def __init__(self,dataset_config:DictConfig):
         self.dataset_config = dataset_config
-        self.tokenizer = self.build_tokenizer()
+        self.tokenizer = AutoTokenizer.from_pretrained(self.dataset_config.tokenizer,trust_remote_code=True)
         self.dataset = load_dataset(dataset_config.dataset,split=dataset_config.split)
         self.prompter = self.build_prompter()
 
 
-    def build_tokenizer(self):
-        tokenizer_name = self.dataset_config.tokenizer
-        if "glm" in tokenizer_name:
-            tokenizer = AutoTokenizer.from_pretrained(tokenizer_name,trust_remote_code=True)
-        elif "t5" in tokenizer_name:
-            tokenizer = T5Tokenizer.from_pretrained(tokenizer_name)
-        elif "bart" in tokenizer_name:
-            tokenizer = BartTokenizer.from_pretrained(tokenizer_name)
-        else:
-            raise NotImplementedError(f"Tokenizer {tokenizer_name} is not supported")
-        return tokenizer
 
     def build_prompter(self):
         all_prompts = DatasetTemplates(self.dataset_config.dataset)
