99fbe347a7909b1c963096ecee1733df4bc8439d
reign12 <3180105232@zju.edu.cn>
2023-01-03 05:01:51 +0000

    Use tokenizer in collate_fn
    - on-the-fly tokenization
    - better fit in the tokenizer format

--- data.py
+++ data.py
@@ -1,4 +1,4 @@
-from typing import Dict,Tuple
+from typing import Dict,Tuple,List
 from torch import Tensor
 
 from torch.utils.data import Dataset
@@ -12,8 +12,25 @@ from datasets import load_dataset
 # prompt support
 from promptsource.templates import DatasetTemplates
 
-from transformers.tokenization_utils_base import BatchEncoding
-import torch
+class PGDataCollator:
+    def __init__(self,datacollator_config: DictConfig):
+        self.datacollator_config = datacollator_config
+        self.tokenizer = AutoTokenizer.from_pretrained(self.datacollator_config.tokenizer,trust_remote_code=True)
+        self.collator = self.build_collator()
+    def build_collator(self):
+        if "glm" in self.datacollator_config.tokenizer:
+            return self.glm_collator
+        else:
+            raise NotImplementedError("Not implemented yet")
+    def glm_collator(self,batch: List[Tuple[str,str]]) -> Dict[str,Tensor]:
+        prompts,answers = [list(item) for item in zip(*batch)]
+        res = self.tokenizer(prompts,padding=True,truncation=True,max_length=self.datacollator_config.max_length,return_tensors="pt")
+        res = self.tokenizer.build_inputs_for_generation(res,targets=answers,max_gen_length=self.datacollator_config.max_gen_length)
+        return res
+
+    def __call__(self, batch: List[Tuple[str,str]]) -> Dict[str, Tensor]:
+        return self.collator(batch)
+
 class PGDataset(Dataset):
     def __init__(self,dataset_config:DictConfig,split:str):
         """
@@ -23,14 +40,12 @@ class PGDataset(Dataset):
         self.max_length = dataset_config.max_length
         self.max_gen_length = dataset_config.max_gen_length
 
-        self.tokenizer = AutoTokenizer.from_pretrained(self.dataset_config.tokenizer,trust_remote_code=True)
         self.dataset = load_dataset(dataset_config.dataset,split=split)
         self.prompter = self.build_prompter()
         self.answer_prompt = dataset_config.answer_prompt
         self.adapter = self.build_adapter()
 
 
-    
     def build_adapter(self):
         adapter_name = self.dataset_config.tokenizer
         if "glm" in adapter_name:
@@ -43,67 +58,11 @@ class PGDataset(Dataset):
             raise NotImplementedError(f"Adapter {adapter_name} is not supported")
         return adapter
         
-    def glm_adapter(self,prompted_data:Tuple[str,str])->Dict[str,Tensor]:
-        def glm_build_inputs_for_generation(model_input:BatchEncoding, targets:str, max_gen_length:int)->Dict[str,Tensor]:
-            """
-            cp from https://huggingface.co/BAAI/glm-roberta-large/blob/main/tokenization_glm.py
-            however rm line 168 in order to align the tensor shape
-            """
-            mask_ids = self.tokenizer.mask_token_ids
-            input_ids = model_input.input_ids
-            batch_size, seq_length = input_ids.shape[:2]
-            position_id, block_position_id = list(range(seq_length)), [0 for _ in range(seq_length)]
-            position_ids, block_position_ids = [], []
-            labels = None
-            if targets is not None:
-                is_batched = isinstance(targets, (list, tuple))
-                targets = self.tokenizer(targets, add_special_tokens=False, padding=False).input_ids
-                if not is_batched:
-                    targets = [targets]
-                targets = [(target + [self.tokenizer.eop_token_id])[:max_gen_length] for target in targets]
-                # max_gen_length = max(map(len, targets))
-                targets = [[self.tokenizer.sop_token_id] + target + [-100] * (max_gen_length - len(target)) for target in targets]
-                assert len(targets) == len(input_ids)
-                targets = torch.tensor(targets, dtype=input_ids.dtype, device=input_ids.device)
-                labels = torch.cat((input_ids.new_full((batch_size, seq_length), -100), targets[:, 1:]), dim=1)
-            for i in range(batch_size):
-                mask_positions = []
-                for mask_id in mask_ids:
-                    mask_positions += (input_ids[i] == mask_id).nonzero(as_tuple=True)[0].tolist()
-                if not mask_positions:
-                    raise ValueError("Cannot find mask token in the input")
-                mask_positions.sort()
-                mask_pos = mask_positions[0]
-                position_ids.append(position_id + [mask_pos] * max_gen_length)
-                block_position_ids.append(block_position_id + list(range(1, max_gen_length + 1)))
-            position_ids = torch.tensor(position_ids, dtype=input_ids.dtype, device=input_ids.device)
-            block_position_ids = torch.tensor(block_position_ids, dtype=input_ids.dtype, device=input_ids.device)
-            position_ids = torch.stack((position_ids, block_position_ids), dim=1)
-            attention_mask = model_input.attention_mask
-            attention_mask = attention_mask.unsqueeze(1).expand(-1, seq_length + max_gen_length, -1)
-            generation_attention_mask = torch.cat([attention_mask.new_zeros((seq_length, max_gen_length)),
-                                                   torch.tril(attention_mask.new_ones((max_gen_length, max_gen_length)))],
-                                                  dim=0).unsqueeze(0).expand(batch_size, -1, -1)
-            attention_mask = torch.cat((attention_mask, generation_attention_mask), dim=2)
-            attention_mask = attention_mask.unsqueeze(1)
-            if targets is None:
-                input_ids = torch.cat((input_ids, input_ids.new_full((batch_size, 1), self.sop_token_id)), dim=-1)
-            else:
-                input_ids = torch.cat((input_ids, targets[:, :-1]), dim=1)
-            batch = {"input_ids": input_ids, "position_ids": position_ids}
-            if labels is None:
-                batch["generation_attention_mask"] = attention_mask
-            else:
-                batch["attention_mask"] = attention_mask
-                batch["labels"] = labels
-            return BatchEncoding(batch)
-
+    def glm_adapter(self,prompted_data:Tuple[str,str])->Tuple[str,str]:
         prompt,answer = prompted_data
         # add mask token
         prompt += "[MASK]"
-        # tokenize
-        res = self.tokenizer(prompt,padding='max_length',max_length=self.max_length,truncation=True, return_tensors="pt")
-        res = glm_build_inputs_for_generation(res,targets=answer,max_gen_length=self.max_gen_length)
+        res = prompt,answer
         return res
         return res
 
@@ -116,7 +75,7 @@ class PGDataset(Dataset):
 
     def __len__(self)->int:
         return len(self.dataset)
-    def __getitem__(self, index:int)->Dict[str,Tensor]:
+    def __getitem__(self, index:int)->Tuple[str,str]:
         # TODO: format the data using prompt, add mask token based on model, padding based on max_lenght, then pass the tokenizer
         data = self.dataset[index]
         prompted_data = self.prompter.apply(data)
