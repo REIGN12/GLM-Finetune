4cd70f771d5a5bac70ba10092147e3f41e8985f4
reign12 <3180105232@zju.edu.cn>
2023-01-05 13:02:23 +0000

    Add strip to postprocess generation res

--- model.py
+++ model.py
@@ -30,7 +30,7 @@ class PGModel(nn.Module):
         **kwargs)
         res = self.tokenizer.batch_decode(res.tolist())
         pattern = r"<\|startofpiece\|>(.*?)(\Z|<\|endofpiece\|>)"
-        res = [re.search(pattern,txt,re.DOTALL).group(1) for txt in res]
+        res = [re.search(pattern,txt,re.DOTALL).group(1).strip() for txt in res]
         return res
 
     def build_generator(self):
